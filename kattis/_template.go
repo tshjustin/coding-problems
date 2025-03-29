2fe2962dc24fc683a7529f589903d455e7705a9e
tshjustin <j.tann2305@gmail.com>
2025-03-29 11:20:46 +0800

    Added template

---
+++ kattis/_template.go
@@ -0,0 +1,17 @@
+package main 
+
+import (
+	"bufio"      // For efficient I/O operations
+	"fmt"        // For formatted I/O (Printf, Scanf, etc.)
+	"os"         // For interacting with OS functionality
+	"sort"       // For sorting slices and user-defined collections
+	"strconv"    // For string conversion operations (Atoi, ParseFloat, etc.)
+	"strings"    // For string manipulation (Split, Join, Replace, etc.)
+	"math"       // For mathematical functions and constants
+	"container/heap" // For heap (priority queue) implementation
+	"math/big"   // For arbitrary precision arithmetic
+)
+
+func main() {
+	
+}
